142c99c0dda78c01c1986e171435826445b99cde
Dilyan Marinov <rsmarinoff@protonmail.com>
2021-03-13 18:18:15 +0000

    Change key bindings for zooming in and out

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true:bold";
+static char *font2[] = { "NotoColorEmoji:pixelsize=24:antialias=true:autohint=true:bold" };
 static int borderpx = 2;
 
 /*
@@ -186,8 +187,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ ShiftMask,            XK_K,           zoom,           {.f = +1} },
-	{ ShiftMask,            XK_J,           zoom,           {.f = -1} },
+	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
+	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
